479455e6aa4bc39ddd4348f9eb82a99032ad789b
radinmov <radinmovahedi1@gmail.com>
2024-07-16 17:06:26 +0300

    1.0.6

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,7 +1,22 @@
-
 import "./Style.css"
 export const Home = () => {
     return (
-            <p>hello</p>
+            <div className="Header">
+                <div className="Header_inner">
+                    <div className="logo">
+                        <svg src="file:///Users/mov/Downloads/Airtour/media/logo/logo.svg"></svg>
+                    </div>
+                    <div className="menu">
+                        <div>plan & book</div>
+                        <div>Flight schudle </div>
+                        <div>Manage booking</div>
+                        <div>discover</div>
+                        <div>Help</div>
+                    </div>
+                    <div className="profile">
+                        <img src="file:///Users/mov/Downloads/Airtour/media/img/icons8-user-64%20(1).png" />
+                    </div>
+                </div>
+            </div>
     )
 }
